a8eea36dc1585a1c43db8c87ecf07b266be41e2a
Oleg Bulgakov <bulgakov.exceedteam@gmail.com>
2021-04-20 16:18:15 +0300

    Add components for routes change_pass, cloud_settings, controllers

---
+++ src/components/Connectors/Connectors.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Connectors() {
+  return (
+    <>
+      <div className="flex flex-wrap bg-blueGray-100">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          This is connectors route
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+        </div>
+      </div>
+    </>
+  );
+}
